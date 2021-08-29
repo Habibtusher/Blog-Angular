cc72898a817c86082e22d94d3543a6c39099879c
Habibtusher <tusherhrt@gmail.com>
2021-08-29 22:21:44 +0600

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './home/contact/contact.component';
 import { HomeComponent } from './home/home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path:"about",
     component:AboutComponent
+  },
+  {
+    path:"contact",
+    component:ContactComponent
   },
   {
     path:"**",
